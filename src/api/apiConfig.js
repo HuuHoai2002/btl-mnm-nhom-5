ef24af8663d261ff02998a0dc5eb370f79b9ed23
huuhoai2002 <huuhoai0511@gmail.com>
2024-08-24 10:42:51 +0700

    feat: update video url

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -3,10 +3,20 @@ const apiConfig = {
   apiKey: "cc4e15ebd92c1ebe17f53b1e1466a548",
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500${imgPath}`,
-  frameUrl: (type, id, episode) =>
-    `https://www.2embed.to/embed/tmdb/${type}?id=${id}${
-      episode ? `&s=1&e=${episode}` : ""
-    }`,
+  frameUrl: (type, id, episode) => {
+    switch (type) {
+      case "movie": {
+        return `https://www.2embed.cc/embed/${id}`;
+      }
+      case "tv": {
+        return `https://www.2embed.cc/embedtv/${id}s=1&e=${episode}`;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+   ,
 };
 
 export default apiConfig;
